59b18188fbaf293b3b8fec2aa84ea0a9d7637cb7
damatgre <gregdamato2@gmail.com>
2021-06-29 11:32:50 -0400

    set up server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,6 +6,19 @@ const app = express();
 //express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        //Your MySQL username,
+        user: 'root',
+        //Your MySQL password
+        password: 'B3ar5B33t5Battl3',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 //default response for any other request (not found)
 app.use((req, res) => {
